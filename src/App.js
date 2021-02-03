e31969b988cb2cbee06970b2b1f2a0300a1bbc12
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-02-03 21:41:03 +0530

    AntDesign Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import { Layout, Avatar, Menu, Breadcrumb } from 'antd';
+import Title from 'antd/lib/typography/Title';
+import SubMenu from 'antd/lib/menu/SubMenu';
+
+const { Header, Footer, Sider, Content } = Layout;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout>
+        <Header style={{ paddingTop: '20px' }}>
+          <Title style={{ color: 'white', float: 'left' }} level={3}>
+            Ant Design
+          </Title>
+          <Avatar style={{ float: 'right' }} src="./logo512.png" />
+        </Header>
+        <Layout>
+          <Sider>
+            <Menu defaultSelectedKeys={['Dashboard']}>
+              <Menu.Item key="Dashboard">Dashboard</Menu.Item>
+              <SubMenu
+                title={
+                  <span>
+                    <icon type="mail" />
+                    <span>About Us</span>
+                  </span>
+                }
+              >
+                <Menu.ItemGroup key="about" title="Item 1">
+                  <Menu.Item key="1">Option 1</Menu.Item>
+                  <Menu.Item key="2">Option 2</Menu.Item>
+                </Menu.ItemGroup>
+                <Menu.ItemGroup key="g2" title="Item 2">
+                  <Menu.Item key="3">Option 3</Menu.Item>
+                  <Menu.Item key="4">Option 4</Menu.Item>
+                </Menu.ItemGroup>
+              </SubMenu>
+            </Menu>
+          </Sider>
+          <Layout>
+            <Content style={{ padding: '0 50px' }}>
+              <Breadcrumb style={{ margin: '16px 0' }}>
+                <Breadcrumb.Item>Dashboard</Breadcrumb.Item>
+              </Breadcrumb>
+              <div
+                style={{
+                  background: 'white',
+                  padding: '25px',
+                  minHeight: '80vh',
+                }}
+              >
+                Content
+              </div>
+            </Content>
+            <Footer style={{ textAlign: 'center' }}>
+              Ant Design Created by Renu
+            </Footer>
+          </Layout>
+        </Layout>
+      </Layout>
     </div>
   );
 }
